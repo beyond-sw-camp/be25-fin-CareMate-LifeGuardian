ab9b138429a0aae170d3031d63c272b7fc8b6718
horolo1234 <qkrwogk2233@gmail.com>
2026-06-26 15:38:51 +0900

    feat: 영업 성과 지표 보고를 위한 SalesPerformerInfo DTO 추가

--- backend/src/main/java/com/caremate/lifeguardian/admin/dto/response/SalesPerformerInfo.java
+++ backend/src/main/java/com/caremate/lifeguardian/admin/dto/response/SalesPerformerInfo.java
@@ -1,10 +1,15 @@
 package com.caremate.lifeguardian.admin.dto.response;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE) // ◀ Jackson용 기본 생성자 강제 생성
 public class SalesPerformerInfo {
     int rank;
     String employeeId;
