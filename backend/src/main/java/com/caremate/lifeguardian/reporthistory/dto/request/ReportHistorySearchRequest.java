b87750ec423568fb7e93c8c70934fcc8c7162005
Myang09 <xghujjk@gmail.com>
2026-07-02 10:14:47 +0900

    fix: 리포트 내용 수정

--- backend/src/main/java/com/caremate/lifeguardian/reporthistory/dto/request/ReportHistorySearchRequest.java
+++ backend/src/main/java/com/caremate/lifeguardian/reporthistory/dto/request/ReportHistorySearchRequest.java
@@ -3,8 +3,6 @@ package com.caremate.lifeguardian.reporthistory.dto.request;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @Builder
 public class ReportHistorySearchRequest {
@@ -18,10 +16,6 @@ public class ReportHistorySearchRequest {
     private String customerStageCode;
 
     private String keyword;
-
-    private List<String> keywordTerms;
-
-    private String customerStageCode;
 
     private int page;
 
