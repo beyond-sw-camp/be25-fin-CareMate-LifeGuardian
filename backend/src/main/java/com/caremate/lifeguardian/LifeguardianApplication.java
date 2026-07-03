8d5d1672026845078cd3da7bbbc3eff12a89644d
Park jaeha <qkrwogk2233@gmail.com>
2026-07-03 12:39:30 +0900

    Release ready (#103)

--- backend/src/main/java/com/caremate/lifeguardian/LifeguardianApplication.java
+++ backend/src/main/java/com/caremate/lifeguardian/LifeguardianApplication.java
@@ -1,14 +1,25 @@
 package com.caremate.lifeguardian;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class LifeguardianApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
+		System.setProperty("user.timezone", "Asia/Seoul");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(LifeguardianApplication.class, args);
 		// test test
 	}
 
 }
+
+
